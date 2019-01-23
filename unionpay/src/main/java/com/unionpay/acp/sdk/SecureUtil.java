36e3b0663e69d6afc2bf95c8c7a591e05acd0931
baifuhai <993006470@qq.com>
2019-01-23 18:17:51 +0800

    fix

--- unionpay/src/main/java/com/unionpay/acp/sdk/SecureUtil.java
+++ unionpay/src/main/java/com/unionpay/acp/sdk/SecureUtil.java
@@ -1,72 +1,45 @@
-/**
- *
- * Licensed Property to China UnionPay Co., Ltd.
- * 
- * (C) Copyright of China UnionPay Co., Ltd. 2010
- *     All Rights Reserved.
- *
- * 
- * Modification History:
- * =============================================================================
- *   Author         Date          Description
- *   ------------ ---------- ---------------------------------------------------
- *   xshu       2014-05-28     报文加密解密等操作的工具类
- * =============================================================================
- */
 package com.unionpay.acp.sdk;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Signature;
+import java.security.*;
 
 import javax.crypto.Cipher;
 
 import org.apache.commons.codec.binary.Base64;
 import org.bouncycastle.crypto.digests.SM3Digest;
+
 /**
- * 
+ * 报文加密解密等操作的工具类
+ *
  * @ClassName SecureUtil
  * @Description acpsdk安全算法工具类
  * @date 2016-7-22 下午4:08:32
- * 声明：以下代码只是为了方便接入方测试而提供的样例代码，商户可以根据自己需要，按照技术文档编写。该代码仅供参考，不提供编码，性能，规范性等方面的保障
  */
 public class SecureUtil {
-	/**
-	 * 算法常量： SHA1
-	 */
+
 	private static final String ALGORITHM_SHA1 = "SHA-1";
-	/**
-	 * 算法常量： SHA256
-	 */
+
 	private static final String ALGORITHM_SHA256 = "SHA-256";
-	/**
-	 * 算法常量：SHA1withRSA
-	 */
+
 	private static final String BC_PROV_ALGORITHM_SHA1RSA = "SHA1withRSA";
-	/**
-	 * 算法常量：SHA256withRSA
-	 */
+
 	private static final String BC_PROV_ALGORITHM_SHA256RSA = "SHA256withRSA";
+
+	private static final String ALGORITHM_MODE_PADDING = "AES/ECB/PKCS1Padding";
 
 	/**
 	 * sm3计算后进行16进制转换
 	 * 
-	 * @param data
-	 *            待计算的数据
-	 * @param encoding
-	 *            编码
-	 * @return 计算结果
+	 * @param data 待计算的数据
+	 * @param encoding 编码
+	 * @return
 	 */
 	public static String sm3X16Str(String data, String encoding) {
 		byte[] bytes = sm3(data, encoding);
 		StringBuilder sm3StrBuff = new StringBuilder();
 		for (int i = 0; i < bytes.length; i++) {
 			if (Integer.toHexString(0xFF & bytes[i]).length() == 1) {
-				sm3StrBuff.append("0").append(
-						Integer.toHexString(0xFF & bytes[i]));
+				sm3StrBuff.append("0").append(Integer.toHexString(0xFF & bytes[i]));
 			} else {
 				sm3StrBuff.append(Integer.toHexString(0xFF & bytes[i]));
 			}
@@ -77,19 +50,16 @@ public class SecureUtil {
 	/**
 	 * sha1计算后进行16进制转换
 	 * 
-	 * @param data
-	 *            待计算的数据
-	 * @param encoding
-	 *            编码
-	 * @return 计算结果
+	 * @param data 待计算的数据
+	 * @param encoding 编码
+	 * @return
 	 */
 	public static byte[] sha1X16(String data, String encoding) {
 		byte[] bytes = sha1(data, encoding);
 		StringBuilder sha1StrBuff = new StringBuilder();
 		for (int i = 0; i < bytes.length; i++) {
 			if (Integer.toHexString(0xFF & bytes[i]).length() == 1) {
-				sha1StrBuff.append("0").append(
-						Integer.toHexString(0xFF & bytes[i]));
+				sha1StrBuff.append("0").append(Integer.toHexString(0xFF & bytes[i]));
 			} else {
 				sha1StrBuff.append(Integer.toHexString(0xFF & bytes[i]));
 			}
@@ -101,24 +71,20 @@ public class SecureUtil {
 			return null;
 		}
 	}
-	
-	
+
 	/**
 	 * sha256计算后进行16进制转换
 	 * 
-	 * @param data
-	 *            待计算的数据
-	 * @param encoding
-	 *            编码
-	 * @return 计算结果
+	 * @param data 待计算的数据
+	 * @param encoding 编码
+	 * @return
 	 */
 	public static String sha256X16Str(String data, String encoding) {
 		byte[] bytes = sha256(data, encoding);
 		StringBuilder sha256StrBuff = new StringBuilder();
 		for (int i = 0; i < bytes.length; i++) {
 			if (Integer.toHexString(0xFF & bytes[i]).length() == 1) {
-				sha256StrBuff.append("0").append(
-						Integer.toHexString(0xFF & bytes[i]));
+				sha256StrBuff.append("0").append(Integer.toHexString(0xFF & bytes[i]));
 			} else {
 				sha256StrBuff.append(Integer.toHexString(0xFF & bytes[i]));
 			}
@@ -128,78 +94,58 @@ public class SecureUtil {
 	
 	/**
 	 * sha256计算后进行16进制转换
-	 * 
-	 * @param data
-	 *            待计算的数据
-	 * @param encoding
-	 *            编码
-	 * @return 计算结果
+	 *
+	 * @param data 待计算的数据
+	 * @param encoding 编码
+	 * @return
+	 * @throws Exception
 	 */
-	public static byte[] sha256X16(String data, String encoding) {
+	public static byte[] sha256X16(String data, String encoding) throws Exception {
 		byte[] bytes = sha256(data, encoding);
 		StringBuilder sha256StrBuff = new StringBuilder();
 		for (int i = 0; i < bytes.length; i++) {
 			if (Integer.toHexString(0xFF & bytes[i]).length() == 1) {
-				sha256StrBuff.append("0").append(
-						Integer.toHexString(0xFF & bytes[i]));
+				sha256StrBuff.append("0").append(Integer.toHexString(0xFF & bytes[i]));
 			} else {
 				sha256StrBuff.append(Integer.toHexString(0xFF & bytes[i]));
 			}
 		}
-		try {
-			return sha256StrBuff.toString().getBytes(encoding);
-		} catch (UnsupportedEncodingException e) {
-			LogUtil.writeErrorLog(e.getMessage(), e);
-			return null;
-		}
+		return sha256StrBuff.toString().getBytes(encoding);
 	}
 
 	/**
-	 * sha1计算.
-	 * 
-	 * @param datas
-	 *            待计算的数据
-	 * @return 计算结果
+	 * sha1计算
+	 *
+	 * @param data 待计算的数据
+	 * @return
+	 * @throws Exception
 	 */
-	private static byte[] sha1(byte[] data) {
-		MessageDigest md = null;
-		try {
-			md = MessageDigest.getInstance(ALGORITHM_SHA1);
-			md.reset();
-			md.update(data);
-			return md.digest();
-		} catch (Exception e) {
-			LogUtil.writeErrorLog("SHA1计算失败", e);
-			return null;
-		}
+	private static byte[] sha1(byte[] data) throws Exception {
+		MessageDigest md = MessageDigest.getInstance(ALGORITHM_SHA1);
+		md.reset();
+		md.update(data);
+		return md.digest();
 	}
-	
+
 	/**
-	 * sha256计算.
-	 * 
-	 * @param datas
-	 *            待计算的数据
-	 * @return 计算结果
+	 * sha256计算
+	 *
+	 * @param data 待计算的数据
+	 * @return
+	 * @throws Exception
 	 */
-	private static byte[] sha256(byte[] data) {
-		MessageDigest md = null;
-		try {
-			md = MessageDigest.getInstance(ALGORITHM_SHA256);
-			md.reset();
-			md.update(data);
-			return md.digest();
-		} catch (Exception e) {
-			LogUtil.writeErrorLog("SHA256计算失败", e);
-			return null;
-		}
+	private static byte[] sha256(byte[] data) throws Exception {
+		MessageDigest md = MessageDigest.getInstance(ALGORITHM_SHA256);
+		md.reset();
+		md.update(data);
+		return md.digest();
 	}
-	
+
 	/**
-	 * SM3计算.
-	 * 
-	 * @param datas
-	 *            待计算的数据
-	 * @return 计算结果
+	 * SM3计算
+	 *
+	 * @param data 待计算的数据
+	 * @return
 	 */
 	private static byte[] sm3(byte[] data) {
 		SM3Digest sm3 = new SM3Digest();
@@ -212,20 +158,18 @@ public class SecureUtil {
 	/**
 	 * sha1计算
 	 * 
-	 * @param datas
-	 *            待计算的数据
-	 * @param encoding
-	 *            字符集编码
+	 * @param datas 待计算的数据
+	 * @param encoding 字符集编码
 	 * @return
 	 */
-	private static byte[] sha1(String datas, String encoding) {
-		try {
-			return sha1(datas.getBytes(encoding));
-		} catch (UnsupportedEncodingException e) {
-			LogUtil.writeErrorLog("SHA1计算失败", e);
-			return null;
-		}
-	}
+//	private static byte[] sha1(String datas, String encoding) {
+//		try {
+//			return sha1(datas.getBytes(encoding));
+//		} catch (UnsupportedEncodingException e) {
+//			LogUtil.writeErrorLog("SHA1计算失败", e);
+//			return null;
+//		}
+//	}
 	
 	/**
 	 * sha256计算
@@ -236,14 +180,14 @@ public class SecureUtil {
 	 *            字符集编码
 	 * @return
 	 */
-	private static byte[] sha256(String datas, String encoding) {
-		try {
-			return sha256(datas.getBytes(encoding));
-		} catch (UnsupportedEncodingException e) {
-			LogUtil.writeErrorLog("SHA256计算失败", e);
-			return null;
-		}
-	}
+//	private static byte[] sha256(String datas, String encoding) {
+//		try {
+//			return sha256(datas.getBytes(encoding));
+//		} catch (UnsupportedEncodingException e) {
+//			LogUtil.writeErrorLog("SHA256计算失败", e);
+//			return null;
+//		}
+//	}
 
 	/**
 	 * sm3计算
@@ -254,58 +198,37 @@ public class SecureUtil {
 	 *            字符集编码
 	 * @return
 	 */
-	private static byte[] sm3(String datas, String encoding) {
-		try {
-			return sm3(datas.getBytes(encoding));
-		} catch (UnsupportedEncodingException e) {
-			LogUtil.writeErrorLog("SM3计算失败", e);
-			return null;
-		}
-	}
+//	private static byte[] sm3(String datas, String encoding) {
+//		try {
+//			return sm3(datas.getBytes(encoding));
+//		} catch (UnsupportedEncodingException e) {
+//			LogUtil.writeErrorLog("SM3计算失败", e);
+//			return null;
+//		}
+//	}
 
-	/**
-	 * 
-	 * @param privateKey
-	 * @param data
-	 * @return
-	 * @throws Exception
-	 */
-	public static byte[] signBySoft(PrivateKey privateKey, byte[] data)
-			throws Exception {
-		byte[] result = null;
+	public static byte[] signBySoft(PrivateKey privateKey, byte[] data) throws Exception {
 		Signature st = Signature.getInstance(BC_PROV_ALGORITHM_SHA1RSA, "BC");
 		st.initSign(privateKey);
 		st.update(data);
-		result = st.sign();
-		return result;
+		return st.sign();
 	}
 	
-	/**
-	 * @param privateKey
-	 * @param data
-	 * @return
-	 * @throws Exception
-	 */
-	public static byte[] signBySoft256(PrivateKey privateKey, byte[] data)
-			throws Exception {
-		byte[] result = null;
+	public static byte[] signBySoft256(PrivateKey privateKey, byte[] data) throws Exception {
 		Signature st = Signature.getInstance(BC_PROV_ALGORITHM_SHA256RSA, "BC");
 		st.initSign(privateKey);
 		st.update(data);
-		result = st.sign();
-		return result;
+		return st.sign();
 	}
 
-	public static boolean validateSignBySoft(PublicKey publicKey,
-			byte[] signData, byte[] srcData) throws Exception {
+	public static boolean validateSignBySoft(PublicKey publicKey, byte[] signData, byte[] srcData) throws Exception {
 		Signature st = Signature.getInstance(BC_PROV_ALGORITHM_SHA1RSA, "BC");
 		st.initVerify(publicKey);
 		st.update(srcData);
 		return st.verify(signData);
 	}
 	
-	public static boolean validateSignBySoft256(PublicKey publicKey,
-			byte[] signData, byte[] srcData) throws Exception {
+	public static boolean validateSignBySoft256(PublicKey publicKey, byte[] signData, byte[] srcData) throws Exception {
 		Signature st = Signature.getInstance(BC_PROV_ALGORITHM_SHA256RSA, "BC");
 		st.initVerify(publicKey);
 		st.update(srcData);
@@ -315,135 +238,94 @@ public class SecureUtil {
 	/**
 	 * 对数据通过公钥进行加密，并进行base64计算
 	 * 
-	 * @param dataString
-	 *            待处理数据
-	 * @param encoding
-	 *            字符编码
-	 * @param key
-	 *            公钥
+	 * @param dataString 待处理数据
+	 * @param encoding 字符编码
+	 * @param key 公钥
 	 * @return
+	 * @throws Exception
 	 */
-	public static String encryptData(String dataString, String encoding,
-			PublicKey key) {
-		/** 使用公钥对密码加密 **/
-		byte[] data = null;
-		try {
-			data = encryptData(key, dataString.getBytes(encoding));
-			return new String(SecureUtil.base64Encode(data), encoding);
-		} catch (Exception e) {
-			LogUtil.writeErrorLog(e.getMessage(), e);
-			return "";
-		}
+	public static String encryptData(String dataString, String encoding, PublicKey key) throws Exception {
+		byte[] data = encryptData(key, dataString.getBytes(encoding));
+		return new String(SecureUtil.base64Encode(data), encoding);
 	}
 
 	/**
-	 * 对数据通过公钥进行加密，并进行base64计算
+	 * 公钥加密，base64编码
 	 * 
-	 * @param dataString
-	 *            待处理数据
-	 * @param encoding
-	 *            字符编码
-	 * @param key
-	 *            公钥
+	 * @param accNo 待处理数据
+	 * @param encoding 字符编码
+	 * @param key 公钥
 	 * @return
+	 * @throws Exception
 	 */
-	public static String encryptPin(String accNo, String pin, String encoding,
-			PublicKey key) {
-		/** 使用公钥对密码加密 **/
-		byte[] data = null;
-		try {
-			data = pin2PinBlockWithCardNO(pin, accNo);
-			data = encryptData(key, data);
-			return new String(SecureUtil.base64Encode(data), encoding);
-		} catch (Exception e) {
-			LogUtil.writeErrorLog(e.getMessage(), e);
-			return "";
-		}
+	public static String encryptPin(String accNo, String pin, String encoding, PublicKey key) throws Exception {
+		byte[] data = pin2PinBlockWithCardNO(pin, accNo);
+		data = encryptData(key, data);
+		return new String(SecureUtil.base64Encode(data), encoding);
 	}
 	
 	/**
-	 * 通过私钥解密
+	 * base64解码，私钥解密
 	 * 
-	 * @param dataString
-	 *            base64过的数据
-	 * @param encoding
-	 *            编码
-	 * @param key
-	 *            私钥
-	 * @return 解密后的数据
+	 * @param dataString base64过的数据
+	 * @param encoding 编码
+	 * @param key 私钥
+	 * @return
+	 * @throws Exception
 	 */
-	public static String decryptData(String dataString, String encoding,
-			PrivateKey key) {
-		byte[] data = null;
-		try {
-			data = SecureUtil.base64Decode(dataString.getBytes(encoding));
-			data = decryptData(key, data);
-			return new String(data, encoding);
-		} catch (Exception e) {
-			LogUtil.writeErrorLog(e.getMessage(), e);
-			return "";
-		}
+	public static String decryptData(String dataString, String encoding, PrivateKey key) throws Exception {
+		byte[] data = SecureUtil.base64Decode(dataString.getBytes(encoding));
+		data = decryptData(key, data);
+		return new String(data, encoding);
 	}
 
 	/**
-	 * BASE64解码
+	 * base64编码
 	 * 
-	 * @param inputByte
-	 *            待解码数据
-	 * @return 解码后的数据
-	 * @throws IOException
+	 * @param data 待编码数据
+	 * @return
 	 */
-	public static byte[] base64Decode(byte[] inputByte) throws IOException {
-		return Base64.decodeBase64(inputByte);
+	public static byte[] base64Encode(byte[] data) {
+		return Base64.encodeBase64(data);
 	}
 
 	/**
-	 * BASE64编码
-	 * 
-	 * @param inputByte
-	 *            待编码数据
-	 * @return 解码后的数据
-	 * @throws IOException
+	 * base64解码
+	 *
+	 * @param data 待解码数据
+	 * @return
 	 */
-	public static byte[] base64Encode(byte[] inputByte) throws IOException {
-		return Base64.encodeBase64(inputByte);
+	public static byte[] base64Decode(byte[] data) {
+		return Base64.decodeBase64(data);
 	}
 
 	/**
+	 * 加密
 	 * 加密除pin之外的其他信息
 	 * 
 	 * @param publicKey
-	 * @param plainData
+	 * @param data
 	 * @return
 	 * @throws Exception
 	 */
-	private static byte[] encryptData(PublicKey publicKey, byte[] plainData)
-			throws Exception {
-		try {
-			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","BC");
-			cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-			return cipher.doFinal(plainData);
-		} catch (Exception e) {
-			throw new Exception(e.getMessage());
-		}
+	private static byte[] encryptData(PublicKey publicKey, byte[] data) throws Exception {
+		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","BC");
+		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+		return cipher.doFinal(data);
 	}
 
 	/**
+	 * 解密
+	 *
 	 * @param privateKey
-	 * @param cryptPin
+	 * @param data
 	 * @return
 	 * @throws Exception
 	 */
-	private static byte[] decryptData(PrivateKey privateKey, byte[] data)
-			throws Exception {
-		try {
-			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","BC");
-			cipher.init(Cipher.DECRYPT_MODE, privateKey);
-			return cipher.doFinal(data);
-		} catch (Exception e) {
-			LogUtil.writeErrorLog("解密失败", e);
-		}
-		return null;
+	private static byte[] decryptData(PrivateKey privateKey, byte[] data) throws Exception {
+		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","BC");
+		cipher.init(Cipher.DECRYPT_MODE, privateKey);
+		return cipher.doFinal(data);
 	}
 
 	/**
@@ -511,7 +393,7 @@ public class SecureUtil {
 	private static byte[] formatPan(String aPan) {
 		int tPanLen = aPan.length();
 		byte[] tByte = new byte[8];
-		;
+
 		int temp = tPanLen - 13;
 		try {
 			tByte[0] = (byte) 0x00;
@@ -547,31 +429,33 @@ public class SecureUtil {
 		return tByte;
 	}
 	
-	 /**
-    * luhn算法
-    * 
-    * @param number
-    * @return
-    */
-   public static int genLuhn(String number) {
-       number = number + "0";
-       int s1 = 0, s2 = 0;
-       String reverse = new StringBuffer(number).reverse().toString();
-       for (int i = 0; i < reverse.length(); i++) {
-           int digit = Character.digit(reverse.charAt(i), 10);
-           if (i % 2 == 0) {// this is for odd digits, they are 1-indexed in //
-                            // the algorithm
-               s1 += digit;
-           } else {// add 2 * digit for 0-4, add 2 * digit - 9 for 5-9
-               s2 += 2 * digit;
-               if (digit >= 5) {
-                   s2 -= 9;
-               }
-           }
-       }
-       int check = 10 - ((s1 + s2) % 10);
-       if (check == 10)
-           check = 0;
-       return check;
-   }
+	/**
+     * luhn算法
+     *
+     * @param number
+     * @return
+     */
+	public static int genLuhn(String number) {
+        number = number + "0";
+        int s1 = 0, s2 = 0;
+        String reverse = new StringBuffer(number).reverse().toString();
+        for (int i = 0; i < reverse.length(); i++) {
+            int digit = Character.digit(reverse.charAt(i), 10);
+            if (i % 2 == 0) {// this is for odd digits, they are 1-indexed in
+                             // the algorithm
+                s1 += digit;
+            } else {// add 2 * digit for 0-4, add 2 * digit - 9 for 5-9
+                s2 += 2 * digit;
+                if (digit >= 5) {
+                    s2 -= 9;
+                }
+            }
+        }
+        int check = 10 - ((s1 + s2) % 10);
+        if (check == 10) {
+			check = 0;
+		}
+		return check;
+    }
+
 }
